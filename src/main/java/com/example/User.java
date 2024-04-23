144e2ea4e2f4f973f4e4062817d9e225896c65f7
tanyaofei <tan.yaofei@outlook.com>
2024-04-23 11:44:36 +0800

    database.sql

--- src/main/java/com/example/User.java
+++ src/main/java/com/example/User.java
@@ -1,24 +1,17 @@
 package com.example;
 
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
 
 /**
  * @author tanyaofei
  * @since 2024/4/18
  **/
 
-@TableName("user")
 public class User {
 
-    @TableId
     private Long id;
 
-    @TableField
     private String username;
 
-    @TableField
     private String password;
 
     public String getUsername() {
--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -3,7 +3,7 @@ create table user
 (
     id       bigint auto_increment
         primary key,
-    username varchar(32) not null
+    username varchar(32) not null,
+    password char(32)    not null
 );
 
-insert into `user` (username) values ('1'), ('2'), ('3'), ('4'), ('5'), ('6');
